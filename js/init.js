36dfe2b8c058e87bcd2a219860c4fa1444a77b02
Adit K <aditkirtani@gmail.com>
2017-08-03 22:07:48 +0530

    Video added for large desktops only

--- js/init.js
+++ js/init.js
@@ -1,16 +1,8 @@
 (function($){
-	$(function(){
+  $(function(){
 
-	    $('.button-collapse').sideNav();
-	    $('.parallax').parallax();
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
 
-	    $('#modal-button').click(function() {
-	        $('#modal1').modal();
-	    });
-
-	    $('#modal-button2').click(function() {
-	        $('#modal2').modal();
-	    });
-
-	}); // end of document ready
+  }); // end of document ready
 })(jQuery); // end of jQuery name space
