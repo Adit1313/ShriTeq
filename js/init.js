658bc71ed7a86b411ba2ac9f5e9a670f5963efae
Adit K <aditkirtani@gmail.com>
2017-08-22 16:06:41 +0530

    Super responsive video

--- js/init.js
+++ js/init.js
@@ -1,8 +1,16 @@
 (function($){
-  $(function(){
+	$(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+	    $('.button-collapse').sideNav();
+	    $('.parallax').parallax();
 
-  }); // end of document ready
+	    $('#modal-button').click(function() {
+	        $('#modal1').modal();
+	    });
+
+	    $('#modal-button2').click(function() {
+	        $('#modal2').modal();
+	    });
+
+	}); // end of document ready
 })(jQuery); // end of jQuery name space
--- js/test.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$('.navbar').sticky({topSpacing: 0});
-
-	
-});
