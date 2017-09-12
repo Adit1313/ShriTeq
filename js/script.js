3e0725799f41156989fd8719d28fe67c0855f4b6
Adit K <aditkirtani@gmail.com>
2017-09-12 19:55:38 +0530

    Rebirth

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+	$('.preloader-background').delay(1700).fadeOut('slow');
+	
+	$('.preloader-wrapper')
+		.delay(1700)
+		.fadeOut();
+});
+
+$(document).ready(function(){
+    $('#top').sticky({topSpacing: 0});
+
+    $('.prev-btn').click(function() {
+    	$('.carousel').carousel('prev');
+    });
+})
+
+$( window ).load(function() {
+    $('.fillWidth').get(0).play();
+ });
